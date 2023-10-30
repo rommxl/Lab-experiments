63453d25a9487550f8e954134d7389278e0779b2
Bodhisatya Ghosh <rommelghosh@gmail.com>
2023-10-30 16:09:51 +0530

    SC exp 7

--- SC/exp 7/exp 7.c
+++ SC/exp 7/exp 7.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float maxf(float a,float b){
+    if(a>b){
+        return a;
+    }
+    return b;
+}
+
+float minf(float a,float b){
+    if(a<b){
+        return a;
+    }
+    return b;
+}
+
 int main(int argc, char const *argv[])
 {
-    return 0;
+    float *plane = malloc(5 * sizeof(float));
+    float *train = malloc(5 * sizeof(float));
+
+    for(int i = 0; i < 5; i++){
+        printf("Enter plane fuzzy set data: ");
+    }
 }
