b7df89a2c4aca545759adec1e4cca992b92a3d24
Bodhisatya Ghosh <rommelghosh@gmail.com>
2024-04-08 10:18:26 +0530

    ML comparison notebook created

--- ML/Classification comparison/commonFunctions.py
+++ ML/Classification comparison/commonFunctions.py
@@ -8,9 +8,9 @@ def classification_report(ytrue, ypred):
     print("Confusion matrix :")
     print(confusion_matrix(ytrue,ypred))
     print("Accuracy score : {}".format(accuracy_score(ytrue,ypred)*100))
-    print("F1 score : {}".format(f1_score(ytrue,ypred)*100))
-    print("Recall score : {}".format(recall_score(ytrue,ypred)*100))
-    print("Sensitivity score : {}".format(precision_recall_fscore_support(ytrue,ypred)*100))
+    print("F1 score : {}".format(f1_score(ytrue, ypred, average='micro')*100))
+    print("Recall score : {}".format(recall_score(ytrue, ypred, average='micro')*100))
+    print("Sensitivity score : {}".format(precision_recall_fscore_support(ytrue, ypred, average='micro')[0]*100))
 
 def regression_report(ytrue, ypred):
     print("R2 score is: {}".format(r2_score(ytrue,ypred)))
